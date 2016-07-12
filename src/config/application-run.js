41dd4c211cf485a46f9d393b3889a93dfeab9fbe
David East <deast@google.com>
2016-07-12 14:56:43 -0700

    feat(layout): Input Bar layout and app shell

--- src/config/application-run.js
+++ src/config/application-run.js
@@ -11,7 +11,9 @@ export function ApplicationRun($rootScope, $location, $route) {
   // Hide app shell header
   const headerShell = document.getElementById('flex-header-shell');
   const $headerShell = angular.element(headerShell);
+  const inputBarShell = document.getElementById('flx-input-bar-shell');
+  const $inputBarShell = angular.element(inputBarShell);
   $headerShell.remove();
-
+  $inputBarShell.remove();
 }
 ApplicationRun.$inject = ['$rootScope', '$location', '$route'];
